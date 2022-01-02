fb509e77065d2367e49cf072d6ad86ad720eb08a
김도림 <dorim@gimdolim-ui-MacBookPro.local>
2022-01-03 00:35:25 +0900

    initial

---
+++ src/movies/movies.controller.ts
@@ -0,0 +1,49 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
+import { Movie } from './entities/movie.entity';
+import { MoviesService } from './movies.service';
+
+@Controller('movies')
+export class MoviesController {
+  constructor(private readonly movieService: MoviesService) {}
+  @Get()
+  getAll(): Movie[] {
+    return this.movieService.getAll();
+  }
+
+  @Get('search')
+  search(@Query('year') searchYear: string) {
+    return `${searchYear}`;
+  }
+
+  @Get(':id')
+  getOne(@Param('id') id: number): Movie {
+    return this.movieService.getOne(id);
+  }
+
+  @Post()
+  create(@Body() movieData: CreateMovieDTO) {
+    console.log(movieData);
+    return this.movieService.create(movieData);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') movieId: number) {
+    return this.movieService.deleteOne(movieId);
+  }
+
+  @Patch(':id')
+  patch(@Param('id') movieId: number, @Body() updateData: UpdateMovieDTO) {
+    return this.movieService.update(movieId, updateData);
+  }
+}
